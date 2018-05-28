470f19ce3db8526668d9dec6fd75d2a0d063e758
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-28 15:08:45 +0300

    add user authentication

--- neighbor/views.py
+++ neighbor/views.py
@@ -8,4 +8,5 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home/home.html')
+    title = 'Neighborhood Watch'
+    return render(request, 'home/home.html', {"title": title})
